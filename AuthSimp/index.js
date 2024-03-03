2d2f6561c729ab7d30ce6d6fb3a078a5efb2dfab
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-03-03 17:34:19 +0530

    authorizations

---
+++ AuthSimp/index.js
@@ -0,0 +1,23 @@
+const express =require("express");
+const app=express();
+
+app.get("/health",function(req,res){
+    const username=req.headers.username;
+    const password=req.headers.password;
+    const kidneyin=req.query.kidneyin;
+
+    if(username!="abzz" || password!="abzz"){
+        res.status(400).json({"msg":"Something is wrong"})
+        return
+    }
+    
+    if(kidneyin !=1 && kidneyin!=2){
+        res.status(400).json({"msg":"Wrong Input"})
+        return
+    }
+    res.json({
+        msg:"kid is fine"
+    })
+});
+
+app.listen(3000);
