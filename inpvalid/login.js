bf1f81234a791ccf1dcb0e752d8eec1ff672cd55
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-03-04 12:16:43 +0530

    zod update

--- inpvalid/login.js
+++ inpvalid/login.js
@@ -17,8 +17,8 @@ app.post("/health",(req,res)=>{
     if(!vue.success){
         res.status(400).json({msg:"Wrong Input"})
     }
-    console.log(vue)
     res.send(vue);
+    console.log(vue)
 })
 
 app.listen(3000);
