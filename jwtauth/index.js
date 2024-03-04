c79ebeea55fed3638b689303e80da4abebfcbb59
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-03-05 00:05:27 +0530

    authenticate

--- jwtauth/index.js
+++ jwtauth/index.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const jwtPassword = "123456";
 
 const app = express();
-
+app.use(express.json());
 const ALL_USERS = [
   {
     username: "harkirat@gmail.com",
@@ -25,6 +25,13 @@ const ALL_USERS = [
 function userExists(username, password) {
   // write logic to return true or false if this user exists
   // in ALL_USERS array
+  let userExists=false;
+  for(let i=0;i<ALL_USERS.length;i++){
+    if(ALL_USERS[i].username==username && ALL_USERS[i].password==password){
+      userExists=true; 
+    }
+  }
+  return userExists;
 }
 
 app.post("/signin", function (req, res) {
@@ -37,7 +44,7 @@ app.post("/signin", function (req, res) {
     });
   }
 
-  var token = jwt.sign({ username: username }, "shhhhh");
+  var token = jwt.sign({ username: username }, jwtPassword);
   return res.json({
     token,
   });
@@ -45,15 +52,24 @@ app.post("/signin", function (req, res) {
 
 app.get("/users", function (req, res) {
   const token = req.headers.authorization;
-  try {
+  // try {
     const decoded = jwt.verify(token, jwtPassword);
     const username = decoded.username;
     // return a list of users other than this username
-  } catch (err) {
-    return res.status(403).json({
-      msg: "Invalid token",
-    });
-  }
+  // } catch (err) {
+  //   return res.status(403).json({
+  //     msg: "Invalid token",
+  //   });
+  // }
+  res.json({
+    users:ALL_USERS.filter(function(value){
+      if(value.username==username){
+        return false;
+      }else{
+        return true;
+      }
+    })
+  })
 });
 
 app.listen(3000)
