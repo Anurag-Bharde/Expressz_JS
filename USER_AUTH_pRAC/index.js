fce5ce55c1da7f81bc613e1744bad205fa7e8b6b
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-05-02 23:50:59 +0530

    just updated

--- USER_AUTH_pRAC/index.js
+++ USER_AUTH_pRAC/index.js
@@ -6,7 +6,7 @@ const userRout=require("./route/User")
 
 // app.use(bodyParse.json());
 app.use("admin/",)
-//this is just a proof of work anbkc d the assign manet to change something
+//this is just a proof of work anbkc d the assidfsfsgn manet to change something
 
 
 app.listen(3000);
